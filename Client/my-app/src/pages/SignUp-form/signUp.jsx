12827c9cb9f9e1f27dbc29f94eeab7f4feeeae23
Abdul Basit <abdulbasitbasit076@gmail.com>
2023-10-08 12:30:37 +0500

    ..

--- Client/my-app/src/pages/SignUp-form/signUp.jsx
+++ Client/my-app/src/pages/SignUp-form/signUp.jsx
@@ -52,7 +52,7 @@ const signUp = () => {
     else 
     {
       // const postData = await axios.post('http://localhost:3000/register', {
-        const postData = await axios.post('https://mern-logreg-api.vercel.app/register', {
+        const postData = await axios.post('https://mern-logreg-api.vercel.app/signUp/register', {
         name,
         email,
         password,
